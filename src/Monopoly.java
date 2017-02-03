50bb2abc86565b1bf68a3a2db7271b94acac3731
Coswar <callumoshea@hotmail.com>
2017-02-03 13:07:32 +0000

    reorganising folders

---
+++ src/Monopoly.java
@@ -0,0 +1,22 @@
+import java.awt.*;  
+import javax.swing.JFrame;  
+  
+public class Monopoly extends Canvas{  
+      
+    @Override
+    public void paint(Graphics g) {  
+  
+        Toolkit t=Toolkit.getDefaultToolkit();  
+        Image i=t.getImage("images/monopoly.jpg");  
+        g.drawImage(i, 0,0,750,700,this);  
+          
+    }  
+        public static void main(String[] args) {  
+        Monopoly m=new Monopoly();  
+        JFrame f=new JFrame();  
+        f.add(m);  
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        f.setVisible(true);  
+    }  
+  
+}
