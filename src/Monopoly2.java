40cfbcf5a230070f5afc42d05e594f5641983dc7
Coswar <callumoshea@hotmail.com>
2017-02-07 17:59:48 +0000

    commented work and started adding Jtextfield functionality

--- src/Monopoly2.java
+++ src/Monopoly2.java
@@ -9,19 +9,29 @@ public class Monopoly2 extends Canvas{
 	
     public static void main(String[] args) {
     	
+    	//creating the frame and sub containers to store items
         board m =new board();
         JFrame f = new JFrame();
-        JPanel p = new JPanel(new BorderLayout());
-        JTextField t =new JTextField("Hello"); 
+        JPanel board_p = new JPanel(new BorderLayout());
+        JPanel frame_p = new JPanel(new BorderLayout());
+        JPanel text_p = new JPanel(new BorderLayout());
+        
+        //the size of the overall frame
         int width = 1000;
         int height = 1000;
-        p.add(m, FlowLayout.LEFT);
-        f.add(p);
         
+        //implementing a text box
+       /* JTextField t =new JTextField(8);
+        t.setFont(t.getFont().deriveFont(50f));
+        text_p.add(t);*/
         
-       /* t.setAlignmentX(900);
-        t.setAlignmentY(900);
-        p.add(t);*/
+        //add panels to the frame
+        board_p.add(m, FlowLayout.LEFT);
+        frame_p.add(board_p,FlowLayout.LEFT);
+        //frame_p.add(text_p, FlowLayout.RIGHT);
+        f.add(frame_p);
+        
+        //set the size and visibility of the frame
         f.setSize(width, height); 
         f.setVisible(true);
         
---
+++ src/text_box.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+import javax.swing.JTextField; 
+
+
+public class text_box extends Canvas{  
+  
+	private static final long serialVersionUID = 1L;
+	
+	 
+	
+    
+	
+}
