f5cf40432fc2049781e351e0ec1728e5aed80e73
Coswar <callumoshea@hotmail.com>
2017-02-07 15:33:46 +0000

    Adding second class to create text box

--- src/Monopoly2.java
+++ src/Monopoly2.java
@@ -1,21 +1,20 @@
 import java.awt.*;  
-import javax.swing.JFrame;  
+import javax.swing.JFrame;
+import javax.swing.JTextField;  
   
 public class Monopoly2 extends Canvas{  
-      
-    /**
-	 * 
-	 */
+  
 	private static final long serialVersionUID = 1L;
 	@Override
     public void paint(Graphics g) {  
   
         Toolkit t=Toolkit.getDefaultToolkit();  
         Image i=t.getImage("src/images/monopoly.jpg");  
-        g.drawImage(i, 0,0,750,700,this);  
-          
-    }  
-        public static void main(String[] args) {  
+        g.drawImage(i, 0,0,750,700,this);       	
+    }
+	
+    public static void main(String[] args) {
+    	
         Monopoly2 m=new Monopoly2();  
         JFrame f=new JFrame();  
         int width = 765;
@@ -23,10 +22,5 @@ public class Monopoly2 extends Canvas{
         f.add(m);
         f.setSize(width, height); 
         f.setVisible(true);
-        
-        //JFrame textbox = new JFrame();
-        
     }  
-      
-  
 }
