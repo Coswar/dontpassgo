bce36a60dca3c6f45d38f8b96eb8063b2b8b3917
Dáire Fagan <daire.fagan@ucdconnect.ie>
2017-02-08 21:40:53 +0000

    More stylistic changes

--- src/Monopoly2.java
+++ src/Monopoly2.java
@@ -9,36 +9,36 @@ public class Monopoly2 extends Canvas{
 	
     public static void main(String[] args) {
     	  		
-    	//creating the frame,set window title and define the exit 
+    	// Creating the frame,set window title and define the exit 
         JFrame f = new JFrame("Monopoly");
         
-        //when we close the window the program will end
+        // When we close the window the program will end
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        //create JPanels to encompass the various elements and define the layout manager used within each panel
+        // Create JPanels to encompass the various elements and define the layout manager used within each panel
         JPanel board_p = new JPanel(new BorderLayout());
         JPanel frame_p = new JPanel();
         JPanel text_p = new JPanel();
         JPanel text_p2 = new JPanel();
         
-        //define the Layout manager used within the JPanels
+        // Define the Layout manager used within the JPanels
         frame_p.setLayout(new BoxLayout(frame_p, BoxLayout.X_AXIS));
         
-        //import the board image from the board class
+        // Import the board image from the board class
         board board_image = new board();
         
-        //import a text box from text box class
+        // Import a text box from text box class
         text_box text_box_top_right = new text_box();
         
-        //add to a panel and set layout and alignment
+        // Add to a panel and set layout and alignment
         text_p.add(text_box_top_right);
         text_p.setLayout(new BoxLayout(text_p, BoxLayout.X_AXIS));
         
-        //set the size of the text panel
+        // Set the size of the text panel
         text_p.setBounds(10,50,50,50);
         text_p.setAlignmentY(Component.TOP_ALIGNMENT);
         
-        //second text box for testing purposes
+        // Second text box for testing purposes
         text_display text_box_bottom_right = new text_display();
         text_p2.add(text_box_bottom_right);
         text_p2.setLayout(new BoxLayout(text_p2, BoxLayout.X_AXIS));
@@ -47,7 +47,7 @@ public class Monopoly2 extends Canvas{
         // work
         
         
-        //colour in frames to see boundries in order to size correctly
+        // Colour in frames to see boundaries in order to size correctly
         frame_p.setBackground(Color.green);
         board_p.setBackground(Color.black);
         text_p.setBackground(Color.red);
@@ -59,10 +59,10 @@ public class Monopoly2 extends Canvas{
         frame_p.add(text_p);
         frame_p.add(text_p2);
          	
-        //add the frame-panel to the Jframe 
+        // Add the frame-panel to the Jframe 
         f.add(frame_p);
         
-        //set the size and visibility of the frame
+        // Set the size and visibility of the frame
         f.setResizable(false);
         f.setSize(1315, 638);  
         f.setVisible(true);
