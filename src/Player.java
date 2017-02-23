9bc158dd00f0cde9896d29b10c2037588d502174
Dáire Fagan <daire.fagan@ucdconnect.ie>
2017-02-23 13:29:27 +0000

    Added getters and setters to Player class for balance, name, & position.

--- src/Player.java
+++ src/Player.java
@@ -25,5 +25,28 @@ public class Player {
 	public int getPosition () {
 		return position;
 	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	
+	
+	
 	
 }
