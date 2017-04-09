f39cae505ec97946cc3aec54857030326bdd5268
ciara <ciara@DESKTOP-89AB5V9>
2017-04-09 23:50:33 +0100

    starting implementation of cards

---
+++ src/Cards.java
@@ -0,0 +1,39 @@
+public class Cards{
+	public String[] community_chest = {
+		"Advance to go",
+		"Go back to Mediterranian Avenue",
+		"Go to jail",
+		"Pay hospital 100",
+		"Doctor's fee. Pay 50",
+		"Pay your insurance premium of 50",
+		"Bank error in your favour. Collect 200",
+		"Annuity matures. Collect 100",
+		"You inherit 100",
+		"From sale of stock you get 50",
+		"Receive interest on 7% preference shares: 25",
+		"Income tax refund. Collect 20",
+		"You have won second prize in a beauty contest. Collect 10",
+		"It is your birthday. Collect 10 from each player",
+		"Get out of jail free",
+		"pay fine of 10 or take a chance"
+	};
+	
+	public String[] chance = {
+		"Advance to go",
+		"Advance to Charles place",
+		"Go to jail",
+		"Take a trip to Reading railroad",
+		"Advance to Illinois Avenue",
+		"Advance to boardwalk",
+		"Go back three spaces",
+		"Make general repairs on all of your houses. For each house pay 25. For each hotel pay 100",
+		"You are assessed for street repairs: 40 per house, 115 per hotel",
+		"Pay school fees of 150",
+		"Drunk in charge fine of 20",
+		"Speeding fine of 15",
+		"Your building loan matures. Recieve 150",
+		"You have won a crossword competition. Collect 100",
+		"Bank pays you dividend of 50",
+		"Get out of jail free"
+		};
+}
