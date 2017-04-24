93e4f1c301edd58d73f815a901d6c5163dc15ee6
ciara <ciara@DESKTOP-89AB5V9>
2017-04-24 21:06:33 +0100

    BACKING CODE IMPLEMENTATION started bot commands

---
+++ Chris's Sprint 5 code including bot/donotpassgo.java
@@ -0,0 +1,57 @@
+
+public class donotpassgo implements Bot {
+	
+	// The public API of YourTeamName must not change
+	// You cannot change any other classes
+	// YourTeamName may not alter the state of the board or the player objects
+	// It may only inspect the state of the board and the player objects
+
+	// check to see if bot has jailcard
+	int jailcard = 0;
+	
+	// check to see if rolled
+	int roll = 0;
+	
+	//array to decide whether or not to buy site
+	//0 is sites that are unbuyable and thus does not change
+	// higher the number greater want to buy site
+	int[] buy_site = {0,6,0,6,0,6,1,0,1,1,0,2,6,2,2,6,3,0,3,3,0,4,0,4,4,6,5,5,6,5,0,6,6,0,
+			6,6,0,7,0,7
+		};
+	
+	//array to decide which site to be mortgaged first
+	// 0 is n/a, 1 is unowned
+	//lower number more allowed to mortgage
+	int[] mortgage_site = {0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,1,1,0,1,1,0,1,0,1,1,1,1,1,1,1,0,1,1,0,
+			1,1,0,1,0,1
+		};
+	
+	donotpassgo (BoardAPI board, PlayerAPI player, DiceAPI dice) {
+		return;
+	}
+	
+	public String getName () {
+		return "donotpassgo";
+	}
+	
+	public String getCommand () {
+		// First call should be to roll
+		//       NEED TO IMPLEMENT FOR DOUBLE ROLL CASE
+		if(roll == 0){
+			roll = 1;
+		return "roll";
+		}
+		
+		
+		
+		//Last call to end turn
+		return "done";
+	}
+	
+	public String getDecision () {
+		// code deciding to pay fine or take chance card
+		// better to take chance
+		return "chance";
+	}
+	
+}
